4175f2f91a7225746bce8c8c2e66261620e7b3d5
ChuYong <jakgon@naver.com>
2023-01-11 16:32:52 +0900

    chore: GitFlow 브랜치 분리 위한 워크플로 로직 추가

--- src/main/kotlin/co/bearus/magcloud/dto/response/EmotionResponseDTO.kt
+++ src/main/kotlin/co/bearus/magcloud/dto/response/EmotionResponseDTO.kt
@@ -2,5 +2,4 @@ package co.bearus.magcloud.dto.response
 
 import co.bearus.magcloud.domain.Emotion
 
-data class EmotionResponseDTO(val emotion: Emotion, val value: Double) {
-}
+data class EmotionResponseDTO(val emotion: Emotion, val value: Double)
