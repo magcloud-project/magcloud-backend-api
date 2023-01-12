5a404074ad6a5be036750b95920f75050ceb4a47
ChuYong <jakgon@naver.com>
2023-01-12 18:27:41 +0900

    feat: DTO 유효성검증 로직 추가

--- src/main/kotlin/co/bearus/magcloud/dto/request/DiaryPatchDTO.kt
+++ src/main/kotlin/co/bearus/magcloud/dto/request/DiaryPatchDTO.kt
@@ -1,10 +1,11 @@
 package co.bearus.magcloud.dto.request
 
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 
 data class DiaryPatchDTO(
     @field:NotEmpty(message = "내용은 비어있을 수 없습니다") val content: String? = null,
-    val date: String? = null,
-    val previousVersion: Long? = null,
+    @field:NotNull(message = "날짜는 비어있을 수 없습니다") @field:NotEmpty(message = "날짜는 비어있을 수 없습니다") val date: String? = null,
+    @field:NotNull(message = "이전 버전이 필요합니다") val previousVersion: Long? = null,
     val force: Boolean? = false
 )
