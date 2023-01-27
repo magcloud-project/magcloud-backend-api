b434691c7100b80253fc732ead9c7b6964cd14e7
ChuYong <jakgon@naver.com>
2023-01-27 21:06:15 +0900

    fix: 리턴 데이트포맷 변경

--- src/main/kotlin/co/bearus/magcloud/SpringWebConfig.kt
+++ src/main/kotlin/co/bearus/magcloud/SpringWebConfig.kt
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.text.SimpleDateFormat
+
 
 @Configuration
 class SpringWebConfig(private val resolver: RequestUserArgumentResolver) : WebMvcConfigurer {
@@ -18,9 +20,11 @@ class SpringWebConfig(private val resolver: RequestUserArgumentResolver) : WebMv
     }
     @Bean
     fun objectMapper(): ObjectMapper{
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         return jacksonObjectMapper()
             .registerModule(KotlinModule())
             .registerModule(JavaTimeModule())
+            .setDateFormat(dateFormat)
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
     }
 }
